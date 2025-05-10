8a9bc90c92962020780855e35634fbde85b14e62
DougAugSilva <douglas.augustosilva323@gmail.com>
2025-05-10 17:04:46 -0300

    atualizando arquivos com a aula de hoje 10/05

--- codigos_sql/5_aplicando_procedures.sql
+++ codigos_sql/5_aplicando_procedures.sql
@@ -1,11 +1,16 @@
 -- EXECUTA PROCEADURES
 USE master;
-GO
+
 EXEC dbo.insere_cliente;
 
-USE master;
-GO
 EXEC dbo.insere_endereco_cliente;
+
+EXEC dbo.insere_nota_fiscal;
+
+EXEC dbo.insere_item_venda;
+
+EXEC dbo.insere_historico_recebimento;
+
 -- ==========================================================
 -- VERIFICA SE OS VALORES FORAM INSERIDOS EM "LIVRARIADB"
 -- ==========================================================
@@ -16,6 +21,15 @@ SELECT * FROM CLIENTE;
 
 -- dbo.insere_endereco_cliente;
 SELECT * FROM ENDERECOS_CLIENTES;
+
+-- dbo.insere_nota_fiscal;
+SELECT * FROM NOTA_FISCAL;
+
+-- dbo.insere_item_venda;
+SELECT * FROM ITEM_VENDA;
+
+-- dbo.insere_historico_recebimento;
+SELECT * FROM HISTORICO_RECEBIMENTO;
 
 -- ==================================================
 -- VERIFICA OS VALORES NO STAGE "STAGE"
