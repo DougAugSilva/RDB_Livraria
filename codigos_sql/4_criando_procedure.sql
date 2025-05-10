58008db0a62da109808b8678552e478e46541861
DougAugSilva <douglas.augustosilva323@gmail.com>
2025-05-10 14:08:21 -0300

    atualizando procedures

--- codigos_sql/4_criando_procedure.sql
+++ codigos_sql/4_criando_procedure.sql
@@ -16,7 +16,7 @@ BEGIN
 		FROM	STAGE.dbo.MOVIMENTACAO_LIVROS
 	
 	DECLARE @nome_cliente		VARCHAR(100);
-	DECLARE @cpf				VARCHAR(14);
+	DECLARE @cpf			VARCHAR(14);
 	DECLARE @email_cliente		VARCHAR(100);
 	DECLARE @telefone_cliente	VARCHAR(20);
 
@@ -38,7 +38,7 @@ BEGIN
 	SET NOCOUNT OFF;
 END;
 -- ========================================================
--- PROCEDURE INSERE CLIENTE ENDERECO DO CLIENTE
+-- PROCEDURE INSERE ENDERECO DO CLIENTE
 USE master;
 GO
 
@@ -56,7 +56,7 @@ BEGIN
 
 	DECLARE @numero_endereco	INT;
 	DECLARE @tipo_endereco		INT;
-	DECLARE @cep				VARCHAR(20);
+	DECLARE @cep			VARCHAR(20);
 	DECLARE @complemento		VARCHAR(100);
 
 
@@ -82,8 +82,116 @@ BEGIN
 END;
 -- ========================================================
 -- PROCEDURE INSERE NOTA FISCAL
+USE master;
+GO
 
+IF OBJECT_ID('dbo.insere_nota_fiscal') IS NOT NULL
+	DROP PROCEDURE dbo.insere_nota_fiscal
 
+CREATE PROCEDURE dbo.insere_nota_fiscal
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE db_cursor CURSOR FOR
+		SELECT NUMERO_NOTA_FISCAL, DATA_VENDA
+		FROM STAGE.dbo.MOVIMENTACAO_LIVROS
+	
+	DECLARE @numero_nota_fiscal 	INT;
+	DECLARE @data_venda		DATE;
+	
+	OPEN db_cursor;
+	FETCH NEXT FROM db_cursor INTO @numero_nota_fiscal, @data_venda;
+
+	WHILE @@FECTH_STATUS = 0
+	BEGIN
+
+		IF NOT EXIST (SELECT NUMERO_NOTA_FISCAL FROM LIVRARIA.dbo.NOTA_FISCAL WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal)
+			INSERT INTO LIVRARIADB.dbo.NOTA_FISCAL (NUMERO_NOTA_FISCAL, DATA_NOTA)
+			VALUES (@numero_nota_fiscal, @data_venda)
+		END
+		FETCH NEXT FROM db_cursor INTO @numero_nota_fiscal, @data_nota;
+	END
+	CLOSE db_cursor;
+	DEALLOCATE db_cursor;
+	SET NOCOUNT OFF;
+END;
+-- ========================================================
+-- PROCEDURE INSERE ITEM VENDA
+USE mater;
+GO
+
+IF OBJECT_ID('dbo.insere_item_venda') IS NOT NULL
+	DROP PROCEDURE db.insere_item_venda
+
+CREATE PROCEDURE dbo.insere_item_venda
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE db_cursor CURSOR FOR
+		SELECT QUANTIDADE
+		FROM MOVIMENTACAO_LIVROS
+
+	DECLARE @quantidade	INT;
+
+	OPEN db_cursor;
+	FETCH NEXT FROM db_cursor INTO @quantidade;
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		INSERT INTO LIVRARIADB.dbo.ITEM_VENDA (QUANTIDADE)
+		VALUES (@quantidade)  
+		FETCH NEXT FROM db_cursor INTO @quantidade
+	END
+	CLOSE db_cursor;
+	DEALLOCATE db_cursor;
+	SET NOCOUNT OFF;
+END;
+-- ========================================================
+-- PROCEDURE INSERE HISTORICO RECEBIMENTO
+USE mater;
+GO
+
+IF OBJECT_ID('dbo.insere_historico_recebimento') IS NOT NULL
+	DROP PROCEDURE db.insere_historico_recebimento
+
+CREATE PROCEDURE db.insere_historico_recebimento
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE db_cursor CURSOR FOR
+		SELECT VALOR_RECEBIDO
+		FROM MOVIMENTACAO_LIVROS
+	
+	DECLARE @valor_recebido	DECIMAL(10,2);
+	
+	OPEN db_cursor;
+	FETCH NEXT FROM db_cursor INTO @valor_recebido;
+	
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		INSERT INTO LIVRARIADB.dbo.HISTORICO_RECEBIMENTO (VALOR_RECEBIDO)
+		VALUES (@valor_recebido)
+		FETCH NEXT FROM db_cursor INTO @valor_recebido
+	END
+	CLOSE db_cursor;
+	DEALLOCATE db_cursor;
+	SET NOCOUNT OFF;
+END;	
+-- ========================================================
+-- PROCEDURE INSERE HISTORICO DIVERGENTE
+USE master;
+GO
+
+IF OBJECT_ID('dbo.insere_historico_divergente') IS NOT NULL
+	DROP PROCEDURE db.insere_historico_divergente
+
+CREATE PROCEDURE db.insere_historico_divergente
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE db_cursor CURSOR FOR .....
+			
+	
 
 
 
@@ -113,19 +221,19 @@ TABELAS A SEREM INSERIDOS OS VALORES
 	-> MOVIMENTACAO_LIVROS.CEP = ENDERECOS_CLIENTES.CEP
 	-> MOVIMENTACAO_LIVROS.COMPLEMENTO = ENDERECOS_CLIENTES.COMPLEMENTO
 
--------------------------------------------------------------------------------------------------
 # NOTA_FISCAL
 	-> MOVIMENTACAO_LIVROS.NUMERO_NOTA_FISCAL = NOTA_FISCAL.NUMERO_NOTA_FISCAL ~verificar valores nulos
 	-> MOVIMENTACAO_LIVROS.DATA_VENDA = NOTA_FISCAL.DATA_NOTA ~verificar valores nulos
+
 
 # ITEM_VENDA
 	-> MOVIMENTACAO_LIVROS.QUANTIDADE = ITEM_VENDA.QUANTIDADE ~verificar valores nulos
 
 # HISTORICO_RECEBIMENTO
 	-> MOVIMENTACAO_LIVROS.VALOR_RECEBIDO = HISTORICO_RECEBIMENTO.VALOR_RECEBIDO	  ~verificar valores nulos
-
+-------------------------------------------------------------------------------------------------
 # HISTORICO_DIVERGENTE
-	-> MOVIMENTACAO_LIVROS.VALOR_TOTAL = HISTORICO_DIVERGENTE.VALOR_TOTAL				~verificar valores nulos
+	-> MOVIMENTACAO_LIVROS.VALOR_TOTAL = HISTORICO_DIVERGENTE.VALOR_TOTAL		    ~verificar valores nulos
 	-> MOVIMENTACAO_LIVROS.CONDICAO_PAGAMENTO = HISTORICO_DIVERGENTE.CONDICAO_PAGAMENTO ~verificar valores nulos
 	-> MOVIMENTACAO_LIVROS.DATA_PROCESSAMENTO = HISTORICO_DIVERGENTE.DATA_PROCESSAMENTO ~verificar valores nulos
 
