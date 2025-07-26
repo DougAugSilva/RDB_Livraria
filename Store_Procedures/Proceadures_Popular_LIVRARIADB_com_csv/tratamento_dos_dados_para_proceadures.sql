2c4b60fe6b039059f712539f45dd4ac28efd8806
DougAugSilva <douglas.augustosilva323@gmail.com>
2025-07-26 15:30:15 -0300

    atualizações da mentoria de hoje

--- Store_Procedures/Proceadures_Popular_LIVRARIADB_com_csv/tratamento_dos_dados_para_proceadures.sql
+++ Store_Procedures/Proceadures_Popular_LIVRARIADB_com_csv/tratamento_dos_dados_para_proceadures.sql
@@ -38,46 +38,42 @@ BEGIN
 
     WHILE @@FETCH_STATUS = 0
 	BEGIN 
-        IF EXISTS (SELECT NUMERO_NOTA_FISCAL FROM STAGE.dbo.VALIDACAO WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal)
+      
+        IF EXISTS (
+            SELECT 1 
+            FROM STAGE.dbo.VALIDACAO 
+            WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal 
+            AND DATA_PROCESSAMENTO = @data_processamento
+            )
+            BEGIN
+                SET @motivo_rejeicao = 'Ja processada'
 
-            SET @motivo_rejeicao = 'Ja processada'
+                INSERT INTO STAGE.dbo.MOVIMENTACAO_LIVROS_REJEITADOS (NOME_CLIENTE_REJEITADOS, NUMERO_ENDERECO_REJEITADOS, COMPLEMENTO_REJEITADOS, CEP_REJEITADOS,
+                TIPO_ENDERECO_REJEITADOS, EMAIL_CLIENTE_REJEITADOS, TELEFONE_CLIENTE_REJEITADOS, CPF_REJEITADOS, NUMERO_NOTA_FISCAL_REJEITADOS, QUANTIDADE_REJEITADOS, 
+                VALOR_ITEM_REJEITADOS, VALOR_TOTAL_REJEITADOS, CONDICAO_PAGAMENTO_REJEITADOS, TITULO_REJEITADOS, AUTOR_REJEITADOS, ID_LOJA_REJEITADOS,
+                ID_ATENDENTE_REJEITADOS, DATA_VENDA_REJEITADOS, DATA_PROCESSAMENTO_REJEITADOS, MOTIVO_REJEICAO)
 
-            INSERT INTO STAGE.dbo.MOVIMENTACAO_LIVROS_REJEITADOS (NOME_CLIENTE_REJEITADOS, NUMERO_ENDERECO_REJEITADOS, COMPLEMENTO_REJEITADOS, CEP_REJEITADOS,
-            TIPO_ENDERECO_REJEITADOS, EMAIL_CLIENTE_REJEITADOS, TELEFONE_CLIENTE_REJEITADOS, CPF_REJEITADOS, NUMERO_NOTA_FISCAL_REJEITADOS, QUANTIDADE_REJEITADOS, 
-            VALOR_ITEM_REJEITADOS, VALOR_TOTAL_REJEITADOS, CONDICAO_PAGAMENTO_REJEITADOS, TITULO_REJEITADOS, AUTOR_REJEITADOS, ID_LOJA_REJEITADOS,
-            ID_ATENDENTE_REJEITADOS, DATA_VENDA_REJEITADOS, DATA_PROCESSAMENTO_REJEITADOS, MOTIVO_REJEICAO)
-
-            VALUES (@nome_cliente, @numero_endereco, @complemento, @cep, 
-            @tipo_endereco, @email_cliente, @telefone_cliente, @cpf, @numero_nota_fiscal, @quantidade,
-            @valor_item, @valor_total, @condicao_pagamento, @titulo, @autor, @id_loja, @id_atendente,
-            @data_venda, @data_processamento, @motivo_rejeicao)
-
-            IF NOT EXISTS (SELECT NUMERO_NOTA_FISCAL FROM STAGE.dbo.VALIDACAO WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal)
-                INSERT INTO STAGE.dbo.VALIDACAO (NUMERO_NOTA_FISCAL, DATA_PROCESSAMENTO)
-                VALUES (@numero_nota_fiscal, @data_processamento)
-
-            DELETE FROM  STAGE.dbo.MOVIMENTACAO_LIVROS WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal
-
-        IF NOT EXISTS (SELECT NUMERO_NOTA_FISCAL FROM STAGE.dbo.VALIDACAO WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal)
-
-            UPDATE STAGE.dbo.MOVIMENTACAO_LIVROS
-            SET TIPO_ENDERECO        =   UPPER(@tipo_endereco)
-            WHERE NUMERO_NOTA_FISCAL =   @numero_nota_fiscal;
-
-             IF NOT EXISTS (SELECT NUMERO_NOTA_FISCAL FROM STAGE.dbo.VALIDACAO WHERE NUMERO_NOTA_FISCAL = @numero_nota_fiscal)
-                 INSERT INTO STAGE.dbo.VALIDACAO (NUMERO_NOTA_FISCAL, DATA_PROCESSAMENTO)
-                 VALUES (@numero_nota_fiscal, @data_processamento)
+                VALUES (@nome_cliente, @numero_endereco, @complemento, @cep, 
+                @tipo_endereco, @email_cliente, @telefone_cliente, @cpf, @numero_nota_fiscal, @quantidade,
+                @valor_item, @valor_total, @condicao_pagamento, @titulo, @autor, @id_loja, @id_atendente,
+                @data_venda, @data_processamento, @motivo_rejeicao)
+            END
+        ELSE
+            BEGIN
+                UPDATE STAGE.dbo.MOVIMENTACAO_LIVROS
+                SET TIPO_ENDERECO        =   UPPER(@tipo_endereco)
+                WHERE NUMERO_NOTA_FISCAL =   @numero_nota_fiscal;
     
-        FETCH NEXT FROM db_cursor INTO  @nome_cliente, @numero_endereco, @complemento, @cep, 
-         @tipo_endereco, @email_cliente, @telefone_cliente, @cpf, @numero_nota_fiscal, @quantidade,
-         @valor_item, @valor_total, @condicao_pagamento, @titulo, @autor, @id_loja, @id_atendente,
-         @data_venda, @data_processamento;
+                FETCH NEXT FROM db_cursor INTO  @nome_cliente, @numero_endereco, @complemento, @cep, 
+                @tipo_endereco, @email_cliente, @telefone_cliente, @cpf, @numero_nota_fiscal, @quantidade,
+                @valor_item, @valor_total, @condicao_pagamento, @titulo, @autor, @id_loja, @id_atendente,
+                @data_venda, @data_processamento;
+            END
     END
     CLOSE db_cursor;
 	DEALLOCATE db_cursor;
+    
 END;
-
-
 
 -- TESTANDO 
 EXEC dbo.tratamento_dados;
@@ -138,3 +134,5 @@ Caso o número esteja nessa tabela, a nota já foi precessada e deve ser rejeita
 3. Os dados rewjeitados dever ser inseridos na tabela STAGE.MOVIMENTACAO_LIVROS_REJEITADOS com o motivo da rejeição
 
 */
+
+-- validar pela data de processamento, se for a mesma data e mesmo numero de nota, inserir
